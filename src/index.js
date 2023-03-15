8f5a9493ee6e2805815c3290e5323de283078756
Amanda <amandaolivcampos@gmail.com>
2023-03-15 15:02:06 -0300

    Primeiro componente

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import "./index.css";
+
+import Primeiro from "./components/basicos/Primeiro";
 
 ReactDOM.render(
   <div>
-    <strong>Ola React</strong>
+    <Primeiro></Primeiro>
   </div>,
   document.getElementById("root")
 );
