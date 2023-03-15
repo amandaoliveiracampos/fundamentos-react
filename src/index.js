c524c8b4a1f8e195d1645f6c5c7e3e6d09f60c07
Amanda <amandaolivcampos@gmail.com>
2023-03-15 14:52:50 -0300

    Exibindo String com JSX

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import ReactDOM from "react-dom";
+import React from "react";
 
-ReactDOM.render("Ola React", document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <strong>Ola React</strong>
+  </div>,
+  document.getElementById("root")
+);
