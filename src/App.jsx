d9fff5ffb1d43c406d645a097d296cb0aabae4bc
Amanda <amandaolivcampos@gmail.com>
2023-03-16 10:31:10 -0300

    Componetes Card #01

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import React from "react";
+import Card from "./components/layout/Card";
+
 import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragemento";
-import Card from "./components/layout/Card";
 
 export default function App() {
   return (
     <div id="app">
       <h1>Fundamentos React</h1>
-      <Card titulo="Exemplo de Card"></Card>
-      <Aleatorio min={10} max={60} />
-      <Fragmento />
-      <ComParametro
-        titulo="Situacao do Aluno"
-        aluno="Gabriel Oliveira"
-        nota={9.3}
-      ></ComParametro>
-      <Primeiro />
+      <Card titulo=" #04 - Desafio Aleatorio">
+        <Aleatorio min={10} max={60} />
+      </Card>
+
+      <Card titulo=" #03 - Fragmento">
+        <Fragmento />
+      </Card>
+
+      <Card titulo=" #02 - Com Parametro">
+        <ComParametro
+          titulo="Situacao do Aluno"
+          aluno="Gabriel Oliveira"
+          nota={9.3}
+        />
+      </Card>
+
+      <Card titulo=" #01 - Primeiro Componente">
+        <Primeiro />
+      </Card>
     </div>
   );
 }
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -1,11 +1,11 @@
-import React from "react";
 import "./Card.css";
+import React from "react";
 
 export default function Card(props) {
   return (
     <div className="Card">
       <div className="Title">{props.titulo}</div>
-      <div className="Content">Conteúdo</div>
+      <div className="Content">{props.children}</div>
     </div>
   );
 }
